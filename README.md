95256a4af2118329765ff65ff12207f25fb176c3
cuiningning <cuiningningxf@163.com>
2018-10-17 19:28:48 +0800

    "完成第876题"

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # leetcode_cpp
 专门用于存放c++编写的leetcode题目
+# src
+存放已经完成 并且提交通过的题目
+# header
+存放头文件
+# util
+存放日常所用工具类
+# ****_Number
+编写过但未提交通过的题目
---
+++ src/MiddleoftheLinkedList_876/MiddleoftheLinkedList_876.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<stdlib.h>
+#include "../../header/ListNode.h"
+using namespace std;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* p;
+        p = head;
+        int count = 0;
+        while(p != NULL){
+            count++;
+            p = p->next;
+}
+        int midPoi = (count/2) + 1;
+        p = head;
+        count = 0;
+        while( p != NULL && ++count < midPoi){
+            p = p->next;
+        }
+        head = p;
+        return head;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    
+    cout << "helloworld"<<endl;
+    system("pause");
+    return 0;
+}
